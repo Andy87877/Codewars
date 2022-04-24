24144ad1f66b2ce77cb5e812dbec4a1aa9ce9f70
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-04-24 14:20:31 +0800

    Create Fundamentals: Return.py

---
+++ Fundamentals: Return.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/python
+def add(a,b):
+    return a+b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+def exponent(a,b):
+    return a**b
+def subt(a,b):
+    return a-b
+    
+# Make more functions. Refer to the description for function names.
+# The code will NOT WORK IF YOU USE names other than the ones
+# from the description
