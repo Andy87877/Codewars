e291bea748655d220e05636588b03f5676ac0168
Andy8787(布長) <andy8787main@gmail.com>
2022-04-27 09:13:20 +0800

    ez

---
+++ Moving Zeros To The End.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+std::vector<int> move_zeroes(const std::vector<int>& input) {
+  vector<int> ans;
+  int zero = 0;
+  for (auto i: input) {
+    if (i != 0) {
+      ans.push_back(i);
+    } else {
+      zero++;
+    }
+  }
+  for (int i = 0; i < zero; i++) {
+    ans.push_back(0);
+  }
+  return ans;
+}
