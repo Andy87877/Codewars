af196af36cb54366e5f2fbc91b2ae5e8f15b8a25
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-04-29 00:05:30 +0800

    Create Grasshopper - Summation.cpp

---
+++ Grasshopper - Summation.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/cpp
+int summation(int num){
+  int total = 0;
+  for (int i = 1; i <= num; i++) {
+    total += i;
+  }
+  return total;
+}
