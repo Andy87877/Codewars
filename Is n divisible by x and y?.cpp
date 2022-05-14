b7b25cbb167dec81c00ecdd5a02ae6b15a517508
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-05-14 18:25:17 +0800

    Delete Is n divisible by x and y?.cpp

--- Is n divisible by x and y?.cpp
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/5545f109004975ea66000086/train/cpp
-bool isDivisible(int n, int x, int y) {
-  if ((n % x == 0) && (n % y == 0)) {
-    return true;
-  } else {
-    return false;
-  }
-}
