99b88526415849a009cbfcc7b949b56bc28b4f93
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-05-14 18:26:07 +0800

    Delete Will you make it?.cpp

--- Will you make it?.cpp
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/cpp
-bool zero_fuel(uint32_t distance_to_pump, uint32_t mpg, uint32_t fuel_left){
- if (distance_to_pump > mpg * fuel_left) {
-   return false;
- } else {
-   return true;
- }
-}
