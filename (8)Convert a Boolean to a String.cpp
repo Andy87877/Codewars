05a665d6c485a9c31f41d9b2c5a50466e52260df
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-05-23 22:17:06 +0800

    Create (8)Convert a Boolean to a String.cpp

---
+++ (8)Convert a Boolean to a String.cpp
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/cpp
+#include <string>
+using namespace std;
+string boolean_to_string(bool b){
+  if (b){
+    return "true";
+  } else {
+    return "false";
+  }
+}
