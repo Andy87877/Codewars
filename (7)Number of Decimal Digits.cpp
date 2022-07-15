031179f4efe7df321da2ccd3c954308a652ae59f
Andy8787(布長) <86054592+Andy87877@users.noreply.github.com>
2022-07-15 11:44:40 +0800

    Create (7)Number of Decimal Digits.cpp

---
+++ (7)Number of Decimal Digits.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int digits(uint64_t n) {
+  string str = to_string(n);
+  return (str.size());
+}
