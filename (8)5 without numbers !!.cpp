3128374407e7216d5160c6d6311fc9e620a2af60
Andy8787(布長) <andy8787main@gmail.com>
2022-10-08 13:53:39 +0800

    Create (8)5 without numbers !!.cpp

---
+++ (8)5 without numbers !!.cpp
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/c
+#include <stdio.h>
+#include <string.h>
+
+int unusual_five(){
+  char ans[] = "hahaa";
+  printf("%d", strlen(ans));
+  return strlen(ans);
+}
