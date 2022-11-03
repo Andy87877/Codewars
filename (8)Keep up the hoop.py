a6e2a4fac1298ee79b15b0e17a37fa5d8dee26f4
Andy8787(布長) <andy8787main@gmail.com>
2022-11-03 22:32:23 +0800

    Create (8)Keep up the hoop.py

---
+++ (8)Keep up the hoop.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/python
+def hoop_count(n):
+    if(n >= 10):
+        return "Great, now move on to tricks"
+    return "Keep at it until you get it"
